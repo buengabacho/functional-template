ed826b08d62d66c00a201312ab7d66a47ef62d27
Rodrigo Zapico <rodri.zapico@gmail.com>
2017-08-23 23:28:03 -0300

    modificado test de ejercicio 2, habia un test repetido y otro con la descripcion incorrecta

--- test/exercise2_test.clj
+++ test/exercise2_test.clj
@@ -6,10 +6,6 @@
   (testing "Tests to filter an empty list."
     (is (= (only-greater-than-five '()) '()))))
 
-(deftest only-greater-than-five-test-empty-list
-  (testing "Tests to filter an empty list."
-    (is (= (only-greater-than-five '()) '()))))
-
 (deftest only-greater-than-five-test-less-than-five-elements
   (testing "Tests to filter a list with elements that are less than five."
     (is (= (only-greater-than-five '(1 2 3 4)) '()))))
@@ -19,5 +15,5 @@
     (is (= (only-greater-than-five '(5)) '()))))	
 	
 (deftest only-greater-than-five-test-greater-than-five-elements
-  (testing "Tests to filter a list with elements that are less than five."
+  (testing "Tests to filter a list with elements that are more than five."
     (is (= (only-greater-than-five '(6 7)) '(6 7)))))
